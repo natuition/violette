70dca6145ae699f748c01aa47a6260d54b5f17c9
avc94 <>
2019-03-10 18:20:09 +0200

    Python connector server and little smoothie connector improvements

--- connectors.py
+++ connectors.py
@@ -2,20 +2,22 @@
 
 import telnetlib
 import re
+import socket
+from threading import Thread
+import traceback
 
 
 class SmoothieConnector:
 
-    def __init__(self, verbose=True, allow_comments=False):
-        self._ip = None
+    def __init__(self, host, verbose=False, allow_comments=False):
+        self._host = host
         self._allow_comments = allow_comments
         self._verbose = verbose
         self._tn = None
 
-    def connect(self, ip):
+    def connect(self):
         try:
-            self._ip = ip
-            self._tn = telnetlib.Telnet(self._ip)
+            self._tn = telnetlib.Telnet(self._host)
             # read Smoothie startup prompt
             self._tn.read_until("Smoothie command shell".encode('ascii'))
             return True
@@ -50,7 +52,7 @@ class SmoothieConnector:
 
             if len(command) > 0:
                 if self._verbose:
-                    print("Streaming g-code to " + self._ip)
+                    print("Streaming g-code to " + self._host)
 
                 self._tn.write(command.encode('ascii') + b"\n")
 
@@ -72,11 +74,61 @@ class SmoothieConnector:
             return False
 
 
+class PythonConnectorServer:
+
+    def __init__(self, host, port, bufsize=4096, verbose=False):
+        self._host = host
+        self._port = port
+        self._buffer_size = bufsize
+        self._verbose = verbose
+        self._socket = socket.socket()
+        self._socket.bind((self._host, self._port))
+        self._incoming_connection = None
+        self._incoming_address = None
+
+    def _on_exception(self):
+        try:
+            self._incoming_connection.close()
+        except:
+            pass
+
+    def start(self, shared_resourse: list):
+        try:
+            if self._verbose:
+                print("Waiting for client connection...")
+
+            self._socket.listen(1)
+            self._incoming_connection, self._incoming_address = self._socket.accept()
+
+            if self._verbose:
+                print("Incoming connection from " + str(self._incoming_address))
+
+            while True:
+                received_data = self._incoming_connection.recv(self._buffer_size).decode()
+
+                if not received_data:
+                    if self._verbose:
+                        print("Connection was closed from the other side.")
+                    self._incoming_connection.close()
+                    break
+
+                shared_resourse.append(received_data)
+
+        except KeyboardInterrupt:
+            self._on_exception()
+            exit()
+        except Exception:
+            tb = traceback.format_exc()
+            print(tb)
+        finally:
+            self._on_exception()
+
+
 def _test_SmoothieConnector():
     g_code = "G0 X10 Y5 F200"
-    ip = "192.168.1.222"
-    sc = SmoothieConnector()
-    if sc.connect(ip):
+    host = "192.168.1.222"
+    sc = SmoothieConnector(verbose=True)
+    if sc.connect(host):
         print("Connection established.")
         if sc.send(g_code):
             print("Code executed successfully.")
@@ -84,5 +136,32 @@ def _test_SmoothieConnector():
         print("Connection closed normally.")
 
 
+def _test_PythonConnectorServer():
+    def writer(data_list: list):
+        while True:
+            if len(data_list) > 0:
+                item = data_list.pop(0)
+                if item == "stop":
+                    print("Stopping writer thread.")
+                    break
+                else:
+                    print(item)
+
+    host = "127.0.0.1"
+    port = 8081
+
+    data_list = list()
+    pcs = PythonConnectorServer(host, port, verbose=True)
+
+    t1 = Thread(target=pcs.start, args=(data_list,))
+    t2 = Thread(target=writer, args=(data_list,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+
 if __name__ == "__main__":
-    pass
+    _test_PythonConnectorServer()
