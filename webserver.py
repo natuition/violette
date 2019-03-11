0248915e850df5840682a85d9a9f88b2bc5a0e52
avc94 <>
2019-03-11 17:35:24 +0200

    Webserver small editing

--- webserver.py
+++ webserver.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from connectors import PythonConnectorClient as PCC
+from connectors import PythonConnectorClient
 
 pcc_host = "127.0.0.1"
 pcc_port = 8081
-pcc = PCC(pcc_host, pcc_port, verbose=True)
-pcc.connect()
-
 web_host = "192.168.8.101"
 web_port = 8080
+
+pcc = PythonConnectorClient(pcc_host, pcc_port, verbose=True)
+pcc.connect()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -27,7 +27,8 @@ def messageReceived(methods=['GET', 'POST']):
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     s = str('G0 X' + str(json.get('X')) + ' Y' + str(json.get('Y')) + ' F100')
-    print(s)
+    print("Got from HTML: " + s)
+
     if str(json.get('Y')) != 'None' and str(json.get('Y')):
         pcc.send(s)
         response = pcc.receive()
