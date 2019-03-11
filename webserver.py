025b6273bb8dbf606964047e34806475fd84fc87
avc94 <>
2019-03-11 21:17:05 +0200

    Direct connection

--- webserver.py
+++ webserver.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from connectors import PythonConnectorClient
+from connectors import SmoothieConnector
 
-pcc_host = "127.0.0.1"
-pcc_port = 9090
+sm_host = "192.168.1.222"
 
 web_host = "192.168.8.101"
 web_port = 8080
 
-pcc = PythonConnectorClient(pcc_host, pcc_port)
-pcc.connect()
+smc = SmoothieConnector(sm_host, True)
+smc.connect()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -32,13 +31,11 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
 
     if str(json.get('Y')) != 'None' and str(json.get('Y')):
         print("Sending g-code...")
-        pcc.send(s)
-        print("Waiting for answer...")
-        response = pcc.receive()
+        response = smc.send_recv(s)
         print("Got answer: " + response)
-        socketio.emit('my response', response, callback=messageReceived)
+        socketio.emit('my response', s + ": " + response, callback=messageReceived)
 
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host=web_host, port=web_port)
-    pcc.disconnect()
+    smc.disconnect()
