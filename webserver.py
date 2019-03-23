520e086c09522651298ed43c8314e95acb40e6ca
avc94 <>
2019-03-23 20:10:24 +0200

    Temporalily disabled smoothie connection

--- webserver.py
+++ webserver.py
@@ -4,11 +4,12 @@ from connectors import SmoothieConnector
 
 sm_host = "192.168.1.222"
 
-web_host = "192.168.8.101"
+# web_host = "192.168.8.101"
+web_host = "127.0.0.1"
 web_port = 8080
 
-smc = SmoothieConnector(sm_host, True)
-smc.connect()
+# smc = SmoothieConnector(sm_host, True)
+# smc.connect()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -30,12 +31,15 @@ def handle_my_custom_event(json, methods=['GET', 'POST']):
     print("Got from HTML: " + s)
 
     if str(json.get('Y')) != 'None' and str(json.get('Y')):
+        """
         print("Sending g-code...")
         response = smc.send_recv(s)
         print("Got answer: " + response)
+        """
+        response = "ok"
         socketio.emit('my response', s + ": " + response, callback=messageReceived)
 
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host=web_host, port=web_port)
-    smc.disconnect()
+    # smc.disconnect()
