45d7f8590c47f618d96993058668d5dd5f640552
avc94 <>
2019-03-24 12:37:40 +0200

    Validation, more informative msges

--- webserver.py
+++ webserver.py
@@ -2,14 +2,21 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from connectors import SmoothieConnector
 
-sm_host = "192.168.1.222"
+x_current = 0
+y_current = 0
+x_max = 10
+y_max = 10
+f_max = 100
 
+sm_host = "192.168.1.222"
 # web_host = "192.168.8.101"
 web_host = "127.0.0.1"
 web_port = 8080
 
 # smc = SmoothieConnector(sm_host, True)
 # smc.connect()
+
+# SET HERE SMOOTHIE STARTING POSITION MANUALLY IF NEEDED
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
@@ -18,23 +25,38 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def sessions():
-    return render_template('test.html')
+    return render_template('test.html', step_max=x_max, force_max=f_max)
 
 
 @socketio.on('command')
 def on_command(params, methods=['GET', 'POST']):
-    # check if keys missed or data corrupted
-    if "X" not in params or "Y" not in params or "F" not in params \
-            or params["X"] == "None" or params["Y"] == "None" or params["F"] == "None" \
-            or params["F"] <= 0 or (params["X"] == 0 and params["Y"] == 0):
-        print("Received unacceptable value(s), g-code wasn't sent to smoothie.")
-        socketio.emit('response', "Received unacceptable value(s), g-code wasn't sent to smoothie.")
+    # check if keys missed
+    if "X" not in params or "Y" not in params or "F" not in params:
+        error_msg = "Some keys are missed, g-code wasn't sent to smoothie."
+        print(error_msg)
+        socketio.emit('response', error_msg)
+        return
+    # check if empty data stored
+    if params["X"] == "None" or params["Y"] == "None" or params["F"] == "None":
+        error_msg = "Some keys contain no data, g-code wasn't sent to smoothie."
+        print(error_msg)
+        socketio.emit('response', error_msg)
+        return
+    # check for zeros
+    if params["F"] <= 0 or (params["X"] == 0 and params["Y"] == 0):
+        error_msg = "F <= 0, or X and Y == 0 at once, g-code wasn't sent to smoothie."
+        print(error_msg)
+        socketio.emit('response', error_msg)
         return
 
     # here should be values correcting (i.e. if we got X-shift 100 but max is 10 - we have to change x to 10
     # ...
 
-    g_code = "G0 X" + str(params["X"]) + " Y" + str(params["Y"]) + " F" + str(params["F"])
+    x = params["X"]
+    y = params["Y"]
+    f = params["F"]
+
+    g_code = "G0 X" + str(x) + " Y" + str(y) + " F" + str(f)
     print("Got from HTML: ", params["X"], params["Y"], params["F"])
     print("Converted to g-code: " + g_code)
 
