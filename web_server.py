b4e542e3df58354bd640c0487d961c9feae280c7
avc94 <>
2019-04-05 19:45:44 +0300

    Coordinates borders checking (have a problem with flask scope)

--- web_server.py
+++ web_server.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from connectors import SmoothieConnector
 
-# corkscrew moving limitations
-# DO NOT CHANGE!
+# constants
+# DO NOT CHANGE MIN AND MAX LIMITATIONS!
 X_MIN = 0
 X_MAX = 198
 Y_MIN = 0
 Y_MAX = 79
 Z_MIN = 0
 Z_MAX = 52
+F_MIN = 1
 F_MAX = 100
+NOT_SENT_MSG = "g-code wasn't sent to smoothie."
 
 sm_host = "192.168.1.222"
 web_port = 8080
@@ -18,16 +20,29 @@ web_port = 8080
 web_host = "127.0.0.1"
 
 #smc = SmoothieConnector(sm_host, True)
+#print("Connecting to smoothie...")
 #smc.connect()
 
-# SET HERE SMOOTHIE STARTING POSITION MANUALLY IF NEEDED
+# TO DO: MOVE CORKSCREW TO STARTING POSITION
+#
 
 x_current = 0
 y_current = 0
+z_current = 0
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
+
+
+def send_response(msg):
+    print(msg)
+    socketio.emit('response', msg)
+
+
+def cur_coords_str():
+    global x_current, y_current, z_current
+    return "current coordinates: X: {0}, Y: {1}, Z: {2}, ".format(x_current, y_current, z_current)
 
 
 @app.route('/')
@@ -37,40 +52,77 @@ def sessions():
 
 @socketio.on('command')
 def on_command(params, methods=['GET', 'POST']):
-    # check if keys missed
-    if "X" not in params or "Y" not in params or "F" not in params:
-        error_msg = "Some keys are missed, g-code wasn't sent to smoothie."
-        print(error_msg)
-        socketio.emit('response', error_msg)
+    g_code = "G0 "
+
+    # check and add X key (x axis)
+    if "X" in params:
+        x = params["X"]
+        if x is None or x == "None":
+            send_response("X key is present but value is None, " + NOT_SENT_MSG)
+            return
+        if x_current + x >= X_MAX:
+            send_response("Corkscrew will go beyond the right border (X_MAX), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        if x_current + x <= X_MIN:
+            send_response("Corkscrew will go beyond the left border (X_MIN), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        x_current += x
+        g_code += "X" + str(x) + " "
+
+    # check and add Y key (y axis)
+    if "Y" in params:
+        y = params["Y"]
+        if y is None or y == "None":
+            send_response("Y key is present but value is None, " + NOT_SENT_MSG)
+            return
+        if y_current + y >= Y_MAX:
+            send_response("Corkscrew will go beyond the front border (Y_MAX), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        if y_current + y <= Y_MIN:
+            send_response("Corkscrew will go beyond the back border (Y_MIN), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        y_current += y
+        g_code += "Y" + str(y) + " "
+
+    # check and add Z key (z axis)
+    if "Z" in params:
+        z = params["Z"]
+        if z is None or z == "None":
+            send_response("Z key is present but value is None, " + NOT_SENT_MSG)
+            return
+        if z_current + z >= Z_MAX:
+            send_response("Corkscrew will go beyond the upper border (Z_MAX), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        if z_current + z <= Z_MIN:
+            send_response("Corkscrew will go beyond the down border (Y_MIN), " + cur_coords_str() + NOT_SENT_MSG)
+            return
+        z_current += z
+        g_code += "Z" + str(z) + " "
+
+    # check and add F key (force)
+    if "F" in params:
+        f = params["F"]
+        if f is None or f == "None":
+            send_response("F key is present but value is None, " + NOT_SENT_MSG)
+            return
+        if f > F_MAX:
+            send_response("Force value is too big ({0} max), ".format(F_MAX) + NOT_SENT_MSG)
+            return
+        if f < F_MIN:
+            send_response("Force value is too low ({0} min), ".format(F_MIN) + NOT_SENT_MSG)
+            return
+        g_code += "F" + str(f)
+    else:
+        send_response("F key is missed, " + NOT_SENT_MSG)
         return
 
-    x = params["X"]
-    y = params["Y"]
-    f = params["F"]
+    print("Converted to g-code: " + g_code + ", sending...")
 
-    # check if empty data stored
-    if x == "None" or y == "None" or f == "None":
-        error_msg = "Some keys contain no data, g-code wasn't sent to smoothie."
-        print(error_msg)
-        socketio.emit('response', error_msg)
-        return
-    # check for zeros
-    if f <= 0 or (x == 0 and y == 0):  # WHAT IF FLOAT ???
-        error_msg = "F <= 0, or X and Y == 0 at once, g-code wasn't sent to smoothie."
-        print(error_msg)
-        socketio.emit('response', error_msg)
-        return
+    # stub for testing without smoothie
+    response = "ok (USING STUB INSTEAD OF SMOOTHIE CONNECTION)"
 
-    # here should be values correcting (i.e. if we got X-shift 100 but max is 10 - we have to change x to 10
-    # ...
-
-    g_code = "G0 X" + str(x) + " Y" + str(y) + " F" + str(f)
-
-    print("Got from HTML: ", params["X"], params["Y"], params["F"])
-    print("Converted to g-code: " + g_code)
-
-    print("Sending g-code...")
     """
+    # uncomment that block if you using smoothie
     smc.send(g_code)
     response = None
     while True:
@@ -78,9 +130,8 @@ def on_command(params, methods=['GET', 'POST']):
         if response != ">".encode("ascii"):
             break
     """
-    response = "ok (stub message)"
-    print("Got answer: " + response)
 
+    print("Got answer: " + response)
     socketio.emit('response', g_code + ": " + response)
 
 
