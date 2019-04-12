d6aa51df7660fc8b23e37190ef223015c0f1c4dd
avc94 <>
2019-04-12 17:56:04 +0300

    Vladimir's interface changings (in-work)

--- static/scripts/control.js
+++ static/scripts/control.js
@@ -107,3 +107,28 @@ socket.on('response', function(msg) {
 		$('div.message_holder').append('<div>' + msg + '</div>');
     }
 });
+
+// control tabs switch handler
+function on_tab_btn(event, tab_name) {
+  // Declare all variables
+  let i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tab_name).style.display = "block";
+  event.currentTarget.className += " active";
+}
+
+// open default "opened" tab on page load
+document.getElementById("default-opened-tab").click();
