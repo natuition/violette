4a73a7e2db9492ea842d479a51b50dddb16e809f
avc94 <>
2019-04-24 20:56:53 +0300

    Global and local config system

--- web_server.py
+++ web_server.py
@@ -2,30 +2,25 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 from connectors import SmoothieConnector
 from multiprocessing import Value
+import json
 
-# global constants
-# DO NOT CHANGE MIN AND MAX LIMITATIONS!
-X_MIN = 0
-X_MAX = 198
-Y_MIN = 0
-Y_MAX = 79
-Z_MIN = 0
-Z_MAX = 52
-F_MIN = 1
-F_MAX = 1000
+CONFIG_GLOBAL_PATH = "config/config_global.json"
+CONFIG_LOCAL_PATH = "config/config_local.json"
+
+# load configuration from config files
+with open(CONFIG_GLOBAL_PATH, "r") as file:
+    config_global = json.loads(file.read())
+with open(CONFIG_LOCAL_PATH, "r") as file:
+    config_local = json.loads(file.read())
+
 NOT_SENT_MSG = "g-code wasn't sent to smoothie."
-
-SMOOTHIE_HOST = "169.254.232.224"
-WEB_SERV_PORT = 8080
-WEB_SERV_HOST = "192.168.8.100"  # for testing using smoothie
-#WEB_SERV_HOST = "127.0.0.1"  # for local testing
 
 x_current = Value('i', 0)
 y_current = Value('i', 0)
 z_current = Value('i', 0)
 engines_enabled = Value('i', 1)
 
-smc = SmoothieConnector(SMOOTHIE_HOST, True)
+smc = SmoothieConnector(config_local["SMOOTHIE_HOST"], True)
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socket_io = SocketIO(app)
@@ -51,49 +46,41 @@ def switch_to_relative():
     return read_until_not(">")
 
 
-def move_until_stopper(axis: str, direction: int):
-    # send move command on X axis
-    smc.send("G0 {0}{1} F100".format(axis, direction))
-    # wait until stopper hit
-    read_until_contains("M999")
-    # halt state needs M999 command to renew working state
-    smc.send("M999")
-    read_until_contains("ok")
+def calibrate_axis(axis_current: Value, axis_label, axis_min_key, axis_max_key):
+    distanse = 1000
+    with axis_current.get_lock():
+        if config_local["X_AXIS_CALIBRATION_TO_MAX"]:
+            smc.send("G28 {0}{1}".format(axis_label, distanse))
+            read_until_contains("ok")
+            axis_current.value = config_local[axis_max_key] - config_local["AFTER_CALIBRATION_AXIS_OFFSET"]
+        else:
+            smc.send("G28 {0}{1}".format(axis_label, -distanse))
+            read_until_contains("ok")
+            axis_current.value = config_local[axis_min_key] + config_local["AFTER_CALIBRATION_AXIS_OFFSET"]
+
+        # set fresh current coordinates on smoothie too
+        smc.send("G92 {0}{1}".format(axis_label, axis_current.value))
+        read_until_contains("ok")
 
 
 def corkscrew_to_start_pos():
-    # move to X axis stopper and wait for ok
-    print("Moving to X axis stopper...")
-    #move_until_stopper("X", -1000)
-    smc.send("G28 X-1000")
-    read_until_contains("ok")
-    print("Ok")
+    # X axis calibration
+    if config_local["USE_X_AXIS_CALIBRATION"]:
+        print("X axis calibration...", end=" ")
+        calibrate_axis(x_current, "X", "X_MIN", "X_MAX")
+        print("Ok")
 
-    # move to Y axis stopper and wait for ok
-    print("Moving to Y axis stopper...")
-    #move_until_stopper("Y", 1000)
-    smc.send("G28 Y1000")
-    read_until_contains("ok")
-    print("Ok")
+    # Y axis calibration
+    if config_local["USE_Y_AXIS_CALIBRATION"]:
+        print("Y axis calibration...", end=" ")
+        calibrate_axis(y_current, "Y", "Y_MIN", "Y_MAX")
+        print("Ok")
 
-    # move to Z axis stopper and wait for ok
-    '''
-    print("Moving to Z axis stopper...")
-    move_until_stopper("Z", 1000)
-    print("Ok")
-    
-    with z_current.get_lock():
-        z_current.value = Z_MAX
-    smc.send("G92 Z{0}".format(z_current.value))
-    read_until_contains("ok")
-    '''
-    with x_current.get_lock():
-        x_current.value = X_MIN+15
-    with y_current.get_lock():
-        y_current.value = Y_MAX-15
-    smc.send("G92 X{0} Y{1}".format(x_current.value, y_current.value))
-    read_until_contains("ok")
-    
+    # Z axis calibration
+    if config_local["USE_Z_AXIS_CALIBRATION"]:
+        print("Z axis calibration...", end=" ")
+        calibrate_axis(z_current, "Z", "Z_MIN", "Z_MAX")
+        print("Ok")
 
 
 def send_response(msg):
@@ -111,7 +98,7 @@ def validate_moving_key(params, key_name, key_min, key_max, current_value):
     if params[key_name] is None or params[key_name] == "None":
         return "{0} key is present but value is None, ".format(key_name) + NOT_SENT_MSG
     if current_value + params[key_name] > key_max:
-        return "Command with {0}{1} goes beyond max acceptable range of {0}_MAX = {2}, "\
+        return "Command with {0}{1} goes beyond max acceptable range of {0}_MAX = {2}, " \
                    .format(key_name, params[key_name], key_max) + cur_coords_str() + ", " + NOT_SENT_MSG
     if current_value + params[key_name] < key_min:
         return "Command with {0}{1} goes beyond min acceptable range of {0}_MIN = {2}, " \
@@ -126,7 +113,7 @@ def extraction_move_cmd_handler(params):
         send_response("F key is missed, " + NOT_SENT_MSG)
         return
     else:
-        error_msg = validate_moving_key(params, "F", F_MIN, F_MAX, 0)
+        error_msg = validate_moving_key(params, "F", config_local["F_MIN"], config_local["F_MAX"], 0)
         if not (error_msg is None):
             send_response(error_msg)
             return
@@ -140,7 +127,7 @@ def extraction_move_cmd_handler(params):
 
     # check and add X key (x axis)
     if "X" in params:
-        error_msg = validate_moving_key(params, "X", X_MIN, X_MAX, x_current.value)
+        error_msg = validate_moving_key(params, "X", config_local["X_MIN"], config_local["X_MAX"], x_current.value)
         if error_msg is None:
             with x_current.get_lock():
                 x_current.value += params["X"]
@@ -151,7 +138,7 @@ def extraction_move_cmd_handler(params):
 
     # check and add Y key (y axis)
     if "Y" in params:
-        error_msg = validate_moving_key(params, "Y", Y_MIN, Y_MAX, y_current.value)
+        error_msg = validate_moving_key(params, "Y", config_local["Y_MIN"], config_local["Y_MAX"], y_current.value)
         if error_msg is None:
             with y_current.get_lock():
                 y_current.value += params["Y"]
@@ -162,7 +149,7 @@ def extraction_move_cmd_handler(params):
 
     # check and add Z key (z axis)
     if "Z" in params:
-        error_msg = validate_moving_key(params, "Z", Z_MIN, Z_MAX, z_current.value)
+        error_msg = validate_moving_key(params, "Z", config_local["Z_MIN"], config_local["Z_MAX"], z_current.value)
         if error_msg is None:
             with z_current.get_lock():
                 z_current.value += params["Z"]
@@ -174,14 +161,11 @@ def extraction_move_cmd_handler(params):
     g_code += "F" + str(params["F"])
     print("Converted to g-code: " + g_code + ", sending...")
 
-    # COMMENT IF USING SMOOTHIE (stub for testing without real smoothie connection)
-    #response = "ok (USING STUB INSTEAD OF REAL SMOOTHIE CONNECTION)"
-
-    # UNCOMMENT THAT BLOCK IF YOU USING SMOOTHIE
-    #"""
-    smc.send(g_code)
-    response = read_until_not(">")
-    #"""
+    if config_local["USE_SMOOTHIE_CONNECTION_SIMULATION"]:
+        response = "ok (SIMULATING SMOOTHIE CONNECTION!)"
+    else:
+        smc.send(g_code)
+        response = read_until_not(">")
 
     send_response(g_code + ": " + response + ", " + cur_coords_str())
 
@@ -228,26 +212,20 @@ def on_command(params, methods=['GET', 'POST']):
 
 
 def main():
-    # UNCOMMENT THIS BLOCK IF USING SMOOTHIE
-    #'''
-    print("Connecting to smoothie...")
-    smc.connect()
-    switch_to_relative()
-    #'''
-
-    # UNCOMMENT THIS BLOCK ONLY (!) IF STOPPERS ARE INSTALLED AND ENABLED, OR ENGINES MAY CRASH!
+    if not config_local["USE_SMOOTHIE_CONNECTION_SIMULATION"]:
+        print("Connecting to smoothie...", end=" ")
+        smc.connect()
+        switch_to_relative()
+        print("Ok")
 
     corkscrew_to_start_pos()
-    
 
-    socket_io.run(app, debug=True, host=WEB_SERV_HOST, port=WEB_SERV_PORT)
+    socket_io.run(app, debug=True, host=config_local["WEB_SERVER_HOST"], port=config_local["WEB_SERVER_PORT"])
 
-    # UNCOMMENT THIS BLOCK IF USING SMOOTHIE
-    #'''
-    print("Disconnecting from smoothie...")
-    smc.disconnect()
-    print("Done.")
-    #'''
+    if not config_local["USE_SMOOTHIE_CONNECTION_SIMULATION"]:
+        print("Disconnecting from smoothie...", end=" ")
+        smc.disconnect()
+        print("Done.")
 
 
 if __name__ == '__main__':
